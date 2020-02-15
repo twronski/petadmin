db4f4002895692cf5445e2dc92e104648edf1e50
Thiago Wronski <wronski79@gmail.com>
2020-02-15 15:54:30 -0300

    Generate Resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :sells
+    resources :campaigns
+    resources :discounts
+    resources :services
+    resources :products
+    resources :suppliers
+    resources :clients
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
